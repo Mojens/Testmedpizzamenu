6c8ff20df03f81d6206e65b09b54a1871f9a1907
Mojens <moha135h@stud.kea.dk>
2021-10-14 12:06:39 +0200

    Pizza menu test med menu

--- src/PizzaMenu.java
+++ src/PizzaMenu.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class PizzaMenu {
 
   String name;
@@ -16,22 +18,31 @@ public class PizzaMenu {
     this.description = aDescription;
     this.number = aNumber;
     this.price = aPrice;
+    pizza = new ArrayList<PizzaMenu>();
+
+
+
+    pizza.add(new PizzaMenu(1,   "Vesuvio",    "Tomatsauce, ost, skinke og oregano", 57));
+    pizza.add(new PizzaMenu(2,   "Amerikaner", "tomatsauce, ost, oksefars og oregano", 53));
+    pizza.add(new PizzaMenu(3,   "Cacciatore", "Tomatsauce, ost, pepperoni og oregano", 57));
+    pizza.add(new PizzaMenu(4,   "Carbona",    "Tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano", 63));
+    pizza.add(new PizzaMenu(5,   "Dennis", "Tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano", 65));
+    pizza.add(new PizzaMenu(6,   "Bertil", "Tomatsauce, ost, bacon og oregano", 57));
+    pizza.add(new PizzaMenu(7,   "Silvia", "Tomatsauce, ost, pepperoni, cocktailpølser og oregano", 61));
+    pizza.add(new PizzaMenu(8,   "Victoria", "Tomatsauce, ost, skinke, ananas, champignon, løg og oregano", 61));
+   pizza.add(new PizzaMenu(9,   "Toronfo", "Tomatsauce, ost, skinke, bacon, kebab, chili og oregano", 61));
+    pizza.add(new PizzaMenu(10, "Capricciosa", "Tomatsauce, ost, skinke, champignon og oregano", 61));
+    pizza.add(new PizzaMenu(11, "Hawai", "Tomatsauce, ost, skinke, ananas og oregano", 61));
+    pizza.add(new PizzaMenu(12, "Le Blissola", "Tomatsauce, ost, skinke, rejer og oregano", 57));
+    pizza.add(new PizzaMenu(13, "Venezia", "Tomatsauce, ost, skinke, bacon og oregano", 61));
+    pizza.add(new PizzaMenu(14, "Mafia", "Tomatsauce, ost, pepperoni, bacon, løg og oregano", 61));
+    pizza.add(new PizzaMenu(15, "Igild", "Tomatsauce, ost, pepperoni, kebab, chili, hvidløgs dressing", 75));
+    pizza.add(new PizzaMenu(16, "Zimba", "Tomatsauce, ost, chili", 49));
+   pizza.add(new PizzaMenu(17, "Abdullah","Tomatsauce, ost, kebab, bacon, pebberfrugt, kebab dressing",99));
+
   }
 
-  static PizzaMenu pizza1 = new PizzaMenu(1, "Vesuvio", "Tomatsauce, ost, skinke og oregano", 57);
-  static PizzaMenu pizza2 = new PizzaMenu(2, "Amerikaner", "tomatsauce, ost, oksefars og oregano", 53);
-  static PizzaMenu pizza3 = new PizzaMenu(3, "Cacciatore", "Tomatsauce, ost, pepperoni og oregano", 57);
-  static PizzaMenu pizza4 = new PizzaMenu(4, "Carbona", "Tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano", 63);
-  static PizzaMenu pizza5 = new PizzaMenu(5, "Dennis", "Tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano", 65);
-  static PizzaMenu pizza6 = new PizzaMenu(6, "Bertil", "Tomatsauce, ost, bacon og oregano", 57);
-  static PizzaMenu pizza7 = new PizzaMenu(7, "Silvia", "Tomatsauce, ost, pepperoni, cocktailpølser og oregano", 61);
-  static PizzaMenu pizza8 = new PizzaMenu(8, "Victoria", "Tomatsauce, ost, skinke, ananas, champignon, løg og oregano", 61);
-  static PizzaMenu pizza9 = new PizzaMenu(9, "Toronfo", "Tomatsauce, ost, skinke, bacon, kebab, chili og oregano", 61);
-  static PizzaMenu pizza10 = new PizzaMenu(10, "Vesuvio", "Tomatsauce, ost, skinke og oregano", 57);
-  static PizzaMenu pizza11 = new PizzaMenu(11, "Vesuvio", "Tomatsauce, ost, skinke og oregano", 57);
-  static PizzaMenu pizza12 = new PizzaMenu(12, "Vesuvio", "Tomatsauce, ost, skinke og oregano", 57);
-  static PizzaMenu pizza13 = new PizzaMenu(13, "Vesuvio", "Tomatsauce, ost, skinke og oregano", 57);
-  static PizzaMenu pizza14 = new PizzaMenu(14, "Vesuvio", "Tomatsauce, ost, skinke og oregano", 57);
+
 
 
   public static void showMenu() {
