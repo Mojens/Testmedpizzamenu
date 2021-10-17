fc0be78f61460bedaaeadeadd6892fc50fbaaf01
Mojens <moha135h@stud.kea.dk>
2021-10-17 21:33:02 +0200

    Fixet popularOrder men den skal bare se lidt pænere ud med output måske en toString

--- src/FufilledOrders.java
+++ src/FufilledOrders.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class FufilledOrders {
 
-    public static void doneOrders(){
+    public static void doneOrders() {
         System.out.println("Fuldførte Ordre");
-        for (String find: CurrentOrders.fufilledOrders){
+        for (String find : CurrentOrders.fufilledOrders) {
             System.out.println(find);
         }
     }
 
-    public static int revenue(){
-        TreeMap<Integer, String> reve = new TreeMap<Integer,String>();
+    public static int revenue() {
+        TreeMap<Integer, String> reve = new TreeMap<Integer, String>();
         int sum = 0;
         for (String findAll : CurrentOrders.fufilledOrders) {
             String dkk = findAll.substring(findAll.indexOf("DKK ") + 4);
@@ -23,24 +24,17 @@ public class FufilledOrders {
         return sum;
     }
 
-    public static void popularOrder(){
+
+    public static void popularOrder() {
         TreeMap<Integer, String> afhentningsTid = new TreeMap<Integer, String>();
         for (String findAll : CurrentOrders.fufilledOrders) {
-            String pris = findAll.substring(findAll.indexOf("Order ID: ") + 10);
-            pris = pris.substring(0, pris.indexOf(" "));
-            int priser=Integer.parseInt(pris);
-
             String pizza = findAll.substring(findAll.indexOf("Pizza: ") + 7);
             pizza = pizza.substring(0, pizza.indexOf(" "));
             int pizzaer = Integer.parseInt(pizza);
-            afhentningsTid.put(pizzaer+priser, findAll);
-
-        }
-        CurrentOrders.fufilledOrders = new ArrayList<>(afhentningsTid.values());
-        for (String find : CurrentOrders.fufilledOrders) {
-            System.out.println(find);
+            afhentningsTid.put(pizzaer,findAll);
+            System.out.println(afhentningsTid.values());
 
         }
     }
-
 }
+
